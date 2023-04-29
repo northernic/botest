c3ab8d85b18f976e012154d8afd866c0b6c1a371
solomon <solomon@local.com>
2023-04-29 16:12:23 +0800

    v3

---
+++ config.go
@@ -0,0 +1,53 @@
+package main
+
+type Config struct {
+	DomainName []string `yaml:"domainName"`
+	GroupID    int64    `yaml:"groupID"`
+	BotToken   string   `yaml:"botToken"`
+	Alternate  struct {
+		M1F struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"M1F"`
+		ISE struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"ISE"`
+		ICEX struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"ICEX"`
+		MIAX struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"MIAX"`
+		TGX struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"TGX"`
+		VGX struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"VGX"`
+		Szzg struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"Szzg"`
+		Shop struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"Shop"`
+		JinSha struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"JinSha"`
+		ShangPuJing struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"ShangPuJing"`
+		LSEX struct {
+			Name          string   `yaml:"name"`
+			NewDomainName []string `yaml:"newDomainName"`
+		} `yaml:"LSEX"`
+	} `json:"alternate"`
+}
